6ea5bbfbd1d6fefc82f18fd6fe88b06ffae15473
MrRezoo <rezam578@gmail.com>
2021-05-02 02:14:15 +0430

    build packages

---
+++ pyaparat2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyaparat2", # Replace with your own username
+    version="0.0.1",
+    author="Mr.Rezoo",
+    author_email="rezam578@gmail.com",
+    description="download aparat videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrRezoo/pyaparat.git",
+    install_requires=[
+        'requests', 'beautifulsoup4'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
